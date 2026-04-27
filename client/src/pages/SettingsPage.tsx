3c5fa4a8da0ac0a48608f232aee03d6686f7b4cb
Miringu Dennis <markdennis05771@gmail.com>
2026-04-27 19:28:31 +0300

    Custom Avatar for users

--- client/src/pages/SettingsPage.tsx
+++ client/src/pages/SettingsPage.tsx
@@ -68,7 +68,7 @@ export const SettingsPage = () => {
     try {
       const fileExt = file.name.split('.').pop();
       const fileName = `${user?.id}-${Math.random()}.${fileExt}`;
-      const filePath = `avatars/${fileName}`;
+      const filePath = `${user?.id}/${filename}`;
 
       const { error: uploadError } = await supabase.storage
         .from('profiles')
